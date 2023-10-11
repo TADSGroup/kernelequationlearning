50db56756b176ab0c3b830221377d463b9046b4c
jfelipeosorio <josorior@unal.edu.co>
2023-10-10 21:11:11 -0700

    KS first step of 1-step working with Matern112

--- source/parameter_learning.py
+++ source/parameter_learning.py
@@ -308,7 +308,7 @@ def grid_search_Matern112_2D(x_train,u_train, print_MSE = False):
 
   k = np.linspace(10**-3, 2 , num=k1)
   distances = dist.pdist(x_train) # pairwise distances
-  beta = np.percentile(distances, 50) # median of the pairwise distances
+  beta = np.percentile(distances, 35) # median of the pairwise distances
   # Search space for rho
   rhos = beta*k
   
@@ -460,6 +460,7 @@ def kernel_parameters_Matern112_2D(X_train, U_train, e):
     
     for i in range(m):
         optim_rho[i], optim_lmbd[i] = grid_search_Matern112_2D(X_train[e*i:e*(i+1)], U_train[:,i].reshape(-1,1))
+        #optim_rho[i], optim_lmbd[i] = 0.11, 0.0 # User input
         G = K_2D(Matern_Kernel_112_2D, X_train[e*i:e*(i+1)],X_train[e*i:e*(i+1)],optim_rho[i]) 
         #M = (G + optim_lmbd[i]*jnp.eye(e)) # Using nugget
         M = G # No nugget
