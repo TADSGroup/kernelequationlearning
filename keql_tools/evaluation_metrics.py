7f09188dd29095ef7489cb0f89ebfb85bd41a36e
jfelipeosorio <josorior@unal.edu.co>
2024-05-11 20:10:14 -0700

    created evaluation_metrics.py with table_u_errors fun

---
+++ keql_tools/evaluation_metrics.py
@@ -0,0 +1,26 @@
+import jax.numpy as jnp
+
+
+
+# Computes normalized root squared mse
+def get_nrmse(true,pred):
+    return jnp.sqrt(jnp.mean((true-pred)**2)/jnp.mean(true**2))
+
+# Draw pandas df containing nrmse between pred u's and true u's
+def table_u_errors(xy_fine, u_models, u_sols, vmapped_u_true_functions, all_u_params_init):
+    # Create empty dict to store errors
+    results_dict = dict()
+    for i in range(len(vmapped_u_true_functions)):
+        # Compute u pred from 1.5 step method at xy_fine
+        u_eval_fine = u_models[i].point_evaluate(xy_fine,u_sols[i])
+        # Compute u pred from 2 step(init) method at xy_fine
+        u_eval_fine_init = u_models[i].point_evaluate(xy_fine,all_u_params_init[i])
+        # Compute u true at xy_fine
+        u_true_fine = vmapped_u_true_functions[i](xy_fine)
+        # Compute rnmse's and append to dict
+        results_dict[i]=[float(get_nrmse(u_true_fine,u_eval_fine)),float(get_nrmse(u_true_fine,u_eval_fine_init))]
+    # Create pandas df from dict
+    result_df_u = pd.DataFrame.from_dict(results_dict,orient = 'index',columns = ['1 step','2 step (at initialization)'])
+    # Display the table
+    display(100*result_df_u.round(4))
+    return None
