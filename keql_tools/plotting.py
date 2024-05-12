e410602a868a63d8fa1d23367dc5a1186da1ae17
jfelipeosorio <josorior@unal.edu.co>
2024-05-11 17:35:55 -0700

    added plot_compare

--- keql_tools/plotting.py
+++ keql_tools/plotting.py
@@ -37,3 +37,40 @@ def plot_obs(xy_fine, xy_all, xy_obs, vmapped_funcs, title = None):
         axs[i].set_ylim(-0.1,1.1)
     plt.show()
     return None
+
+def plot_compare_error(xy_fine, vmapped_func_pred, vmapped_func_true, title = None):
+    """
+        Plots the up to three functions the observed values. 
+
+        Args:
+            xy_fine (Array): Pairs of points in fine grid.
+            xy_all (Array): Pairs of ghosts points.
+            xy_obs (list): List of pairs of observed points per function.
+            vmapped_funcs (list): List of vectorized functions with vmap.
+            title (str): Title name.
+
+        Returns:
+            None: Plots predicted and true functions and their abs error.   
+
+    """ 
+    
+    plt.figure(figsize=(13,4))
+    plt.suptitle(title)
+    plt.subplot(1,3,1)
+    plt.title("Learned u")
+    pred_vals = vmapped_func_pred(xy_fine)
+    plt.tricontourf(xy_fine[:,0],xy_fine[:,1], pred_vals, 200)
+
+    plt.subplot(1,3,2)
+    plt.title("True u")
+    true_vals = vmapped_func_true(xy_fine)
+    plt.tricontourf(xy_fine[:,0],xy_fine[:,1], true_vals, 200)
+
+    plt.subplot(1,3,3)
+    plt.title("Error")
+    plt.tricontourf(xy_fine[:,0],xy_fine[:,1], true_vals-pred_vals,250)
+    plt.colorbar()
+    plt.scatter(xy_obs1[:,0],xy_obs1[:,1],c='red',s = 50)
+    plt.scatter(xy_all[:,0],xy_all[:,1],c='black',s = 4)
+    plt.show()
+    return None
