b6d7d6953ae5948f25fede463439cbab5fabeeb0
Alex Hsu <owlx@uw.edu>
2024-07-14 10:59:48 -0700

    Added some examples for learning kernel with marginal likelihood

--- keql_tools/LM_Solve.py
+++ keql_tools/LM_Solve.py
@@ -124,17 +124,6 @@ def LevenbergMarquadtMinimize(
     return params,loss_vals,JtRes,improvement_ratios,alpha_vals,iterate_history
 
 
-def adaptive_refine_solution(params,equation_model,initial_reg = 1e-3):
-    refinement_losses = []
-    refined_params = params.copy()
-    for reg in tqdm(reg_sequence):
-        J = equation_model.jac(refined_params)
-        F = equation_model.F(refined_params)
-        refined_params = refined_params - l2reg_lstsq(J,F,reg)
-        refinement_losses += [equation_model.loss(refined_params)]
-    return refined_params,jnp.array(refinement_losses)
-
-
 @jit
 def l2reg_lstsq(A, y, reg=1e-10):
     U,sigma,Vt = jnp.linalg.svd(A, full_matrices=False)
@@ -175,3 +164,30 @@ def adaptive_refine_solution(params,equation_model,initial_reg = 1e-4,num_iter =
         refinement_losses.append(loss_vals[choice])
         reg_vals.append(reg)
     return refined_params,jnp.array(refinement_losses),jnp.array(reg_vals)
+
+import jax
+def run_jaxopt(solver,x0):
+    state = solver.init_state(x0)
+    sol = x0
+    values,errors,stepsizes = [state.value],[state.error],[state.stepsize]
+    update = lambda sol,state:solver.update(sol,state)
+    jitted_update = jax.jit(update)
+    for iter_num in tqdm(range(solver.maxiter)):
+        sol,state = jitted_update(sol,state)
+        values.append(state.value)
+        errors.append(state.error)
+        stepsizes.append(state.stepsize)
+        if solver.verbose > 0:
+            print("Gradient Norm: ",state.error)
+            print("Loss Value: ",state.value)
+        if state.error<=solver.tol:
+            break
+        if stepsizes[-1]==0:
+            print("Restart")
+            state = solver.init_state(sol)
+    convergence_data = {
+        "values":jnp.array(values),
+        "gradnorms":jnp.array(errors),
+        "stepsizes":jnp.array(stepsizes)
+    }
+    return sol,convergence_data,state
