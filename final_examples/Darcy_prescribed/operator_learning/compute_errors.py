9a16abb9419b9fd904ef4f62729a6d21f1d59aa4
jfelipeosorio <josorior@unal.edu.co>
2025-01-28 14:49:48 -0800

    added new plot of operator learning errors for darcy

--- final_examples/Darcy_prescribed/operator_learning/compute_errors.py
+++ final_examples/Darcy_prescribed/operator_learning/compute_errors.py
@@ -1,6 +1,6 @@
 # imports
 import jax
-jax.config.update("jax_default_device",jax.devices()[2])
+jax.config.update("jax_default_device",jax.devices()[3])
 jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 from jax import jit,grad,jacfwd,jacrev,vmap
