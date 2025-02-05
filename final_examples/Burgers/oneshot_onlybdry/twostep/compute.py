28a67e7b1eca197864c7970bc1d4acac2dc98f81
jfelipeosorio <josorior@unal.edu.co>
2025-02-05 12:05:06 -0800

    added compute notebook of twostep

---
+++ final_examples/Burgers/oneshot_onlybdry/twostep/compute.py
@@ -0,0 +1,277 @@
+import jax
+from jax import jit
+jax.config.update("jax_enable_x64", True)
+# jax.config.update("jax_default_device",jax.devices()[1])
+import numpy as np
+import jax.numpy as jnp
+
+# from KernelTools import *
+from parabolic_data_utils import (
+    build_burgers_data,
+    build_tx_grid_chebyshev,
+)
+
+from KernelTools import diagpart
+
+from EquationModel import CholInducedRKHS, OperatorPDEModel,OperatorModel
+from Kernels import (
+    get_centered_scaled_poly_kernel,
+    get_gaussianRBF,
+    get_anisotropic_gaussianRBF
+)
+from KernelTools import eval_k,dx_k,dxx_k,dt_k
+from KernelTools import vectorize_kfunc
+
+import Optimizers
+import importlib
+importlib.reload(Optimizers)
+from Optimizers import (
+    CholeskyLM,SVD_LM,LMParams,plot_optimization_results
+)
+
+from evaluation_metrics import get_nrmse,get_nmae,compute_results
+from plotting import plot_input_data,plot_compare_error
+
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+plt.style.use('default')
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica",
+    'font.size': 20
+})
+
+# coeffs of Burgers eqn
+kappa = 0.01
+alpha = 0.5
+# IC
+def func_u0(x):
+    u0 = 0.25 * (x * np.sin(1 * np.pi*x) - np.sin(5*np.pi*x) - np.sin(3*np.pi*x))
+    return u0
+
+# solve Burgers eqn
+u_true_function,ut_true_function,interpolant,t_vals,sols = (
+    build_burgers_data(func_u0=func_u0,
+                        kappa = kappa,
+                        alpha = alpha,
+                        k_timestep=0.0001,
+                        n_finite_diff=1999
+                        )
+)
+
+# size of coll grid
+num_grid_x, num_grid_t = 26, 26
+
+# get interior and bdry pts of coll grid
+tx_int,tx_bdy = build_tx_grid_chebyshev([0,1],[0,1],num_grid_t,num_grid_x,alpha = 0.6)
+
+# coll grid
+tx_all = jnp.vstack([tx_bdy,tx_int])
+# u vals at coll grid
+u_all = u_true_function(tx_all)
+
+# idx for IC
+init_obs_inds = jnp.where(tx_int[:,0]==0)[0][::1]
+num_init_obs_inds = len(init_obs_inds)
+# idx for FC
+final_obs_inds = jnp.where(tx_int[:,0]==1)[0][::1]
+# idx for IC + FC
+tx_sample_inds = jnp.hstack([init_obs_inds,final_obs_inds])
+# obs grid
+tx_obs = jnp.vstack([tx_bdy,tx_int[tx_sample_inds]])
+# u vals at obs grid
+u_obs = u_true_function(tx_obs)
+
+# fine grid
+num_fine_grid = 200
+t_fine, x_fine = jnp.meshgrid(jnp.linspace(0,1,num_fine_grid),jnp.linspace(0,1,num_fine_grid))
+tx_fine = jnp.vstack([t_fine.flatten(),x_fine.flatten()]).T
+# u vals at fine grid
+u_true = u_true_function(tx_fine)
+
+# x fine grid
+xfine = jnp.linspace(0,1,num_fine_grid)
+# u vals at fine grid at IC
+u_true_IC = u_true_function(jnp.vstack([0.0*jnp.ones(num_fine_grid), xfine]).T)
+# u vals at fine grid at FC
+u_true_FC = u_true_function(jnp.vstack([1.0*jnp.ones(num_fine_grid), xfine]).T)
+
+# u basis ops
+u_operators = (eval_k,dx_k,dt_k,dxx_k)
+# L ops
+feature_operators = (eval_k,dx_k,dxx_k)
+# u kernel
+k_u = get_anisotropic_gaussianRBF(1.,jnp.diag(jnp.array([1.,100])))
+# u RKHS 
+u_model = CholInducedRKHS(tx_all,
+                          u_operators,
+                          k_u,
+                          nugget_size = 1e-8
+)
+# u fitted params
+u_params_init = u_model.get_fitted_params(tx_obs,u_obs)
+# L's at u
+grid_features_init = (
+    (u_model.evaluate_operators(feature_operators,tx_int,u_params_init))
+    .reshape(
+            len(tx_int),
+            len(feature_operators),
+            order = 'F'
+        )
+)
+# P kernel
+k_P_u_part = get_centered_scaled_poly_kernel(2,
+                                      grid_features_init,
+                                      c=1.,
+                                      scaling = 'diagonal')
+@vectorize_kfunc
+def k_P(x,y):
+    return 10*k_P_u_part(x[2:],y[2:])
+# P RKHS
+P_model = OperatorModel(k_P)
+
+# u and P object
+EqnModel = OperatorPDEModel(P_model,
+                            (u_model,),
+                            (tx_obs,),
+                            (u_obs,),
+                            (tx_int,),
+                            feature_operators,
+                            rhs_operator=dt_k,
+                            datafit_weight = 100
+)
+
+# u_pred vals at fine grid
+u_pred = u_model.point_evaluate(tx_fine,u_params_init)
+
+# u error at fine grid
+print(get_nrmse(u_true,u_pred))
+
+
+
+
+# new IC
+def new_func_u0(x):
+    u0 = -1 * x * np.sin(2 * jnp.pi * x)
+    return u0
+# solve Burgers with new IC
+new_u_true_function,new_ut_true_function,new_interp,t_vals,sols_new = (
+    build_burgers_data(func_u0=new_func_u0,
+                       kappa = kappa,
+                       alpha = alpha,
+                       k_timestep=0.0001,
+                       n_finite_diff=1999)
+)
+
+def get_u_pde_adj(u0_new):
+    # Phat of 2 step method
+    @jit
+    def get_pred_ut_2step(input_features):
+        init_P_features = EqnModel.single_eqn_features(u_model,u_params_init,tx_int)
+        rhs_stacked = EqnModel.stacked_collocation_rhs
+        P_params_naive = P_model.fit_params(init_P_features,rhs_stacked,nugget = 1e-8)
+        return P_model.predict_new(input_features,init_P_features,P_params_naive)
+
+    # Observations at ICs and BCs
+    tx_init_bdy = jnp.vstack([tx_int[tx_int[:,0]==0],tx_bdy])
+    new_obs = u0_new(tx_init_bdy) 
+
+    # Set the PDE model
+    class PdeModel():
+        datafit_weight = 50.
+        @jit
+        def get_grid_features(u_params):
+            evaluation = u_model.evaluate_operators(feature_operators,tx_int,u_params)
+            grid_features = evaluation.reshape(len(tx_int),len(feature_operators),order = 'F')
+            return jnp.hstack([tx_int,grid_features])
+        
+        @jit
+        def get_grid_target(u_params):
+            return u_model.evaluate_operators((dt_k,),tx_int,u_params)
+        
+        @jit
+        def eval_obs_points(u_params):
+            return u_model.point_evaluate(tx_init_bdy,u_params)
+        
+        @jit
+        def datafit_residual(u_params):
+            obs_preds = PdeModel.eval_obs_points(u_params)
+            return new_obs - obs_preds
+        
+        @jit
+        def equation_residual(u_params):
+            P_features = PdeModel.get_grid_features(u_params)
+            P_model_preds = get_pred_ut_2step(P_features)
+            ugrid_target = PdeModel.get_grid_target(u_params)
+            return (ugrid_target - P_model_preds)
+        
+        @jit
+        def F(u_params):
+            eqn_res = PdeModel.equation_residual(u_params)
+            data_res = PdeModel.datafit_residual(u_params)
+            return jnp.hstack([
+                PdeModel.datafit_weight * data_res/jnp.sqrt(len(data_res)),
+                eqn_res/jnp.sqrt(len(eqn_res))
+                ])
+        
+        jac = jit(jax.jacrev(F))
+
+        def loss(u_params):
+            return jnp.linalg.norm(PdeModel.F(u_params))**2
+        
+        @jit
+        def damping_matrix(u_params):
+            return u_model.kmat+1e-5 * diagpart(u_model.kmat)
+
+    ## OPTIMIZE
+
+    optParams = LMParams(
+        max_iter = 251,init_alpha = 0.05,min_alpha = 1e-16,show_progress=True)
+
+    new_u_init = 0*u_params_init
+
+    u_pde_params,convergence_data = CholeskyLM(
+        new_u_init.copy(),
+        PdeModel,
+        beta = 0.,
+        optParams=optParams
+    )
+    svd_params = LMParams(
+        max_iter=201,
+        init_alpha=convergence_data.alpha_vals[-1],
+        min_alpha=1e-16,print_every=50,
+        step_adapt_multiplier=1.6)
+
+
+    u_pde_params_adj,_ = SVD_LM(u_pde_params,PdeModel,beta = 1e-16,optParams=svd_params)
+
+    return u_pde_params_adj 
+
+# solve Burgers with new IC 
+new_u_params = get_u_pde_adj(new_u_true_function)
+
+# new_u vals at fine grid
+new_u_true = new_u_true_function(tx_fine)
+
+# new_u_pred vals at fine grid
+new_u_pred = u_model.point_evaluate(tx_fine,new_u_params)
+
+print(get_nrmse(new_u_true,new_u_pred))
+
+# store data
+data = {'tx_obs': tx_obs,
+        'tx_all': tx_all,
+        'tx_int': tx_int,
+        'tx_bdy': tx_bdy,
+        'tx_fine': tx_fine,
+        'xfine': xfine,
+        'u_true': u_true,
+        'u_pred': u_pred,
+        'u_true_IC': u_true_IC,
+        'u_true_FC': u_true_FC,
+        'new_u_true': new_u_true,
+        'new_u_pred': new_u_pred
+}
+
+# save data
+np.save('data.npy', np.array(data, dtype=object), allow_pickle=True)
